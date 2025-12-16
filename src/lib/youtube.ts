07b50e4c844819da29ffe33434642646f33164e7
shibatani <rto1728@gmail.com>
2025-12-17 02:52:44 +0900

    feat: チャンネルURLに動画セクションを追加

--- src/lib/youtube.ts
+++ src/lib/youtube.ts
@@ -1,3 +1,3 @@
 /** チャンネルURLを生成 */
 export const buildChannelUrl = (channelId: string): string =>
-  `https://www.youtube.com/channel/${channelId}`;
+  `https://www.youtube.com/channel/${channelId}/videos`;
