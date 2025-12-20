dcc9c1373e170f87f70f19606245137fba859d8d
shibatani <rto1728@gmail.com>
2025-12-20 23:09:28 +0900

    feat: 検索ワークフローにキーワード入力を追加し、メイン処理でのキーワード取得方法を更新

--- src/batch/search/index.ts
+++ src/batch/search/index.ts
@@ -71,7 +71,7 @@ const order = searchOrder.viewCount;
 const publishedAfter = dayjs().subtract(6, "month");
 
 const main = async () => {
-  const keyword = sample(SEARCH_KEYWORDS)!;
+  const keyword = process.env.SEARCH_KEYWORD || process.argv[2] || sample(SEARCH_KEYWORDS)!;
   console.log(`🔍 検索キーワード: ${keyword}`);
 
   const { items: searchResults, totalResults } = await searchVideos({
